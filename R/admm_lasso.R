e5ea5cab72775398211cd2a24a1282e86be65442
Yixuan Qiu <yixuan.qiu@cos.name>
2014-12-03 11:03:58 -0500

    possibly better choice of rho

--- R/admm_lasso.R
+++ R/admm_lasso.R
@@ -1,5 +1,5 @@
 admm_lasso = function(x, y, lambda = 1.0, maxit = 500L, eps_abs = 1e-6,
-                      eps_rel = 1e-6, rho = lambda * nrow(x))
+                      eps_rel = 1e-6, rho = 10 * lambda * nrow(x))
 {
     .Call("admm_lasso", as.matrix(x), as.numeric(y), as.numeric(lambda),
           as.integer(maxit), as.numeric(eps_abs), as.numeric(eps_rel),
