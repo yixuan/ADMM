64d4a67e1da8956859a4bda8a025feb611816776
Yixuan Qiu <yixuanq@gmail.com>
2015-01-13 19:18:56 -0500

    move standardization functions to a seperate class

--- src/ADMMLasso.h
+++ src/ADMMLasso.h
@@ -107,4 +107,152 @@ public:
 
 
 
+inline double sd_n(const Eigen::Ref<Eigen::VectorXd> &v)
+{
+    double mean = v.mean();
+    double s = 0.0, tmp = 0.0;
+    int n = v.size();
+    for(int i = 0; i < n; i++)
+    {
+        tmp = v[i] - mean;
+        s += tmp * tmp;
+    }
+    s /= n;
+    return sqrt(s);
+}
+
+class DataStd
+{
+private:
+    typedef Eigen::MatrixXd MatrixXd;
+    typedef Eigen::VectorXd VectorXd;
+    typedef Eigen::ArrayXd ArrayXd;
+    typedef Eigen::Ref<ArrayXd> Ref;
+
+    // flag - 0: standardize = FALSE, intercept = FALSE
+    //             directly fit model
+    // flag - 1: standardize = TRUE, intercept = FALSE
+    //             scale x and y by their standard deviation
+    // flag - 2: standardize = FALSE, intercept = TRUE
+    //             center x, standardize y
+    // flag - 3: standardize = TRUE, intercept = TRUE
+    //             standardize x and y
+    int flag;
+
+    int n;
+    int p;
+
+    double meanY;
+    double scaleY;
+    ArrayXd meanX;
+    ArrayXd scaleX;
+public:
+    DataStd(int n, int p, bool standardize, bool intercept)
+    {
+        this->n = n;
+        this->p = p;
+        this->flag = int(standardize) + 2 * int(intercept);
+        this->meanY = 0.0;
+        this->scaleY = 1.0;
+        
+        switch(flag)
+        {
+            case 1:
+                scaleX.resize(p);
+                break;
+            case 2:
+                meanX.resize(p);
+                break;
+            case 3:
+                meanX.resize(p);
+                scaleX.resize(p);
+                break;
+            default:
+                break;
+        }
+    }
+
+    void standardize(MatrixXd &X, VectorXd &Y)
+    {
+        // standardize Y
+        switch(flag)
+        {
+            case 1:
+                scaleY = sd_n(Y);
+                Y.array() /= scaleY;
+                break;
+            case 2:
+            case 3:
+                meanY = Y.mean();
+                Y.array() -= meanY;
+                scaleY = Y.norm() / sqrt(double(n));
+                Y.array() /= scaleY;
+                break;
+            default:
+                break;
+        }
+
+        // standardize X
+        switch(flag)
+        {
+            case 1:
+                for(int i = 0; i < p; i++)
+                {
+                    scaleX[i] = sd_n(X.col(i));
+                    X.col(i).array() /= scaleX[i];
+                }
+                break;
+            case 2:
+                meanX = X.colwise().mean();
+                for(int i = 0; i < p; i++)
+                {
+                    X.col(i).array() -= meanX[i];
+                }
+                break;
+            case 3:
+                meanX = X.colwise().mean();
+                for(int i = 0; i < p; i++)
+                {
+                    X.col(i).array() -= meanX[i];
+                }
+                scaleX = X.colwise().norm() / sqrt(double(n));
+                for(int i = 0; i < p; i++)
+                {
+                    X.col(i).array() /= scaleX[i];
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    void recover(double &beta0, Ref coef)
+    {
+        switch(flag)
+        {
+            case 0:
+                beta0 = 0;
+                break;
+            case 1:
+                beta0 = 0;
+                coef /= scaleX;
+                coef *= scaleY;
+                break;
+            case 2:
+                coef *= scaleY;
+                beta0 = meanY - (coef * meanX).sum();
+                break;
+            case 3:
+                coef /= scaleX;
+                coef *= scaleY;
+                beta0 = meanY - (coef * meanX).sum();
+                break;
+            default:
+                break;
+        }
+    }
+
+    double get_scaleY() { return scaleY; }
+};
+
 #endif // ADMMLASSO_H
--- src/Lasso.cpp
+++ src/Lasso.cpp
@@ -12,21 +12,6 @@ typedef Eigen::Map<MatrixXd> MapMat;
 typedef Eigen::Map<VectorXd> MapVec;
 typedef Eigen::Map<ArrayXd>  MapArray;
 
-template <typename Derived>
-inline double sd_n(const DenseBase<Derived> &v)
-{
-    double mean = v.mean();
-    double s = 0.0, tmp = 0.0;
-    int n = v.size();
-    for(int i = 0; i < n; i++)
-    {
-        tmp = v[i] - mean;
-        s += tmp * tmp;
-    }
-    s /= n;
-    return sqrt(s);
-}
-
 RcppExport SEXP admm_lasso(SEXP x_, SEXP y_, SEXP lambda_,
                            SEXP standardize_, SEXP intercept_,
                            SEXP opts_)
@@ -44,107 +29,24 @@ BEGIN_RCPP
     int p = datX.cols();
     double lambda = as<double>(lambda_) * n;
     
-    bool standardize = as<bool>(standardize_);
-    bool intercept = as<bool>(intercept_);
-    // flag - 0: standardize = FALSE, intercept = FALSE
-    //             directly fit model
-    // flag - 1: standardize = TRUE, intercept = FALSE
-    //             scale x and y by their standard deviation
-    // flag - 2: standardize = FALSE, intercept = TRUE
-    //             center x, standardize y
-    // flag - 3: standardize = TRUE, intercept = TRUE
-    //             standardize x and y
-    int flag = int(standardize) + 2 * int(intercept);
-    
     List opts(opts_);
     int maxit = as<int>(opts["maxit"]);
     double eps_abs = as<double>(opts["eps_abs"]);
     double eps_rel = as<double>(opts["eps_rel"]);
     double rho = as<double>(opts["rho"]);
 
-    // Standardize datY
-    double meanY = 0.0, scaleY = 1.0;
-    switch(flag)
-    {
-        case 1:
-            scaleY = sd_n(datY);
-            datY.array() /= scaleY;
-            break;
-        case 2:
-        case 3:
-            meanY = datY.mean();
-            datY.array() -= meanY;
-            scaleY = datY.norm() / sqrt(double(n));
-            datY.array() /= scaleY;
-            break;
-        default:
-            break;
-    }
+    bool standardize = as<bool>(standardize_);
+    bool intercept = as<bool>(intercept_);
+    DataStd datstd(n, p, standardize, intercept);
+    datstd.standardize(datX, datY);
     
-    // Standardize datX
-    ArrayXd meanX(p);
-    ArrayXd scaleX(p);
-    switch(flag)
-    {
-        case 1:
-            for(int i = 0; i < p; i++)
-            {
-                scaleX[i] = sd_n(datX.col(i));
-                datX.col(i).array() /= scaleX[i];
-            }
-            break;
-        case 2:
-            meanX = datX.colwise().mean();
-            for(int i = 0; i < p; i++)
-            {
-                datX.col(i).array() -= meanX[i];
-            }
-            break;
-        case 3:
-            meanX = datX.colwise().mean();
-            for(int i = 0; i < p; i++)
-            {
-                datX.col(i).array() -= meanX[i];
-            }
-            scaleX = datX.colwise().norm() / sqrt(double(n));
-            for(int i = 0; i < p; i++)
-            {
-                datX.col(i).array() /= scaleX[i];
-            }
-            break;
-        default:
-            break;
-    }
-    
-    ADMMLasso solver(datX, datY, lambda / scaleY, eps_abs, eps_rel);
+    ADMMLasso solver(datX, datY, lambda / datstd.get_scaleY(), eps_abs, eps_rel);
     solver.init(rho);
     int niter = solver.solve(maxit);
     
     ArrayXd beta(p + 1);
-    MapArray coef(&beta[1], p);
-    coef = solver.get_z();
-    switch(flag)
-    {
-        case 0:
-            beta[0] = 0;
-            break;
-        case 1:
-            beta[0] = 0;
-            coef /= scaleX;
-            coef *= scaleY;
-            break;
-        case 2:
-            coef *= scaleY;
-            beta[0] = meanY - (coef * meanX).sum();
-            break;
-        case 3:
-            coef /= scaleX;
-            coef *= scaleY;
-            beta[0] = meanY - (coef * meanX).sum();
-            break;
-        default:
-            break;
-    }
+    beta.segment(1, p) = solver.get_z();
+    datstd.recover(beta[0], beta.segment(1, p));
     
     return List::create(Named("coef") = beta,
                         Named("niter") = niter);
