6d8af795ae6023cadecc87a58574c74b4b5ad920
Yixuan Qiu <yixuan.qiu@cos.name>
2015-02-02 16:19:55 -0500

    classes for SCAD

--- R/admm_scad.R
+++ R/admm_scad.R
@@ -1,3 +1,63 @@
+## Class to describe a SCAD model
+ADMM_SCAD = setRefClass("ADMM_SCAD",
+                        fields = list(penalty_a = "numeric"),
+                        contains = "ADMM_Lasso"
+)
+
+## Class to store fitting results of SCAD model
+ADMM_SCAD_fit = setRefClass("ADMM_SCAD_fit", contains = "ADMM_Lasso_fit")
+
+
+
+
+
+##### Member functions of ADMM_SCAD #####
+
+## Initialize fields including default values
+ADMM_SCAD$methods(
+    initialize = function(...)
+    {
+        .self$penalty_a = 3.7
+        callSuper(...)
+    }
+)
+
+## Set up penalty parameters
+ADMM_SCAD$methods(
+    penalty = function(lambda = NULL, nlambda = 100, lambda_min_ratio,
+                       penalty_a = 3.7, ...)
+    {
+        if(penalty_a <= 2)
+            stop("penalty_a must be greater than 2")
+        
+        .self$penalty_a = as.numeric(penalty_a)
+        callSuper(lambda, nlambda, lambda_min_ratio, ...)
+        
+        invisible(.self)
+    }
+)
+
+## Fit model and conduct the computing
+ADMM_SCAD$methods(
+    fit = function(...)
+    {
+        res = .Call("admm_scad", .self$x, .self$y, .self$lambda,
+                    .self$nlambda, .self$lambda_min_ratio,
+                    .self$standardize, .self$intercept,
+                    .self$penalty_a,
+                    list(maxit = .self$maxit,
+                         eps_abs = .self$eps_abs,
+                         eps_rel = .self$eps_rel,
+                         rho_ratio = .self$rho_ratio),
+                    PACKAGE = "ADMM")
+        do.call(ADMM_SCAD_fit, res)
+    }
+)
+
+
+
+
+
 admm_scad = function(x, y, lambda = NULL,
                      nlambda = 100,
                      lambda_min_ratio = ifelse(nrow(x) < ncol(x), 0.01, 0.0001),
