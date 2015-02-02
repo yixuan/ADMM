8d7c4b6ebc794f6338d75eb071faa05f90d6c928
Yixuan Qiu <yixuan.qiu@cos.name>
2015-02-02 16:34:24 -0500

    documentation for SCAD

--- R/admm_scad.R
+++ R/admm_scad.R
@@ -58,25 +58,63 @@ ADMM_SCAD$methods(
 
 
 
-admm_scad = function(x, y, lambda = NULL,
-                     nlambda = 100,
-                     lambda_min_ratio = ifelse(nrow(x) < ncol(x), 0.01, 0.0001),
-                     standardize = TRUE, intercept = TRUE,
-                     pen_a = 3.7,
-                     opts = list())
+#' Fitting A Penalized Least Squares Model With SCAD Threshold
+#' 
+#' @description This function will not directly conduct the computation,
+#' but rather returns an object of class "\code{ADMM_SCAD}" that contains
+#' several memeber functions to actually constructs and fits the model.
+#' 
+#' Member functions that are callable from this object are listed below:
+#' 
+#' \tabular{ll}{
+#'   \code{$penalty()}  \tab Specify the penalty parameter. See section
+#'                           \strong{Setting Penalty Parameter} for details.\cr
+#'   \code{$opts()}     \tab Setting additional options. See section
+#'                           \strong{Additional Options} for details.\cr
+#'   \code{$fit()}      \tab Fit the model and do the actual computation.
+#'                           See section \strong{Model Fitting} for details.
+#' }
+#' 
+#' @param x The data matrix
+#' @param y The response vector
+#' @param intercept Whether to fit an intercept in the model. Default is \code{TRUE}.
+#' @param standardize Whether to standardize the explanatory variables before
+#'                    fitting the model. Default is \code{TRUE}. Fitted coefficients
+#'                    are always returned on the original scale.
+#' 
+#' @examples set.seed(123)
+#' n = 100
+#' p = 20
+#' b = runif(p)
+#' x = matrix(rnorm(n * p, mean = 1.2, sd = 2), n, p)
+#' y = 5 + c(x %*% b) + rnorm(n)
+#' 
+#' ## Directly fit the model
+#' admm_scad(x, y)$fit()
+#' 
+#' ## Or, if you want to have more customization:
+#' model = admm_scad(x, y)
+#' print(model)
+#' 
+#' ## Specify the lambda sequence and the "a"-parameter
+#' model$penalty(nlambda = 20, lambda_min_ratio = 0.01, penalty_a = 4)
+#' 
+#' ## Lower down precision for faster computation
+#' model$opts(maxit = 100, eps_rel = 0.001)
+#' 
+#' ## Inspect the updated model setting
+#' print(model)
+#' 
+#' ## Fit the model and do the actual computation
+#' res = model$fit()
+#' res$beta
+#' 
+#' ## Create a solution path plot
+#' res$plot()
+#' 
+#' @author Yixuan Qiu <\url{http://statr.me}>
+#' @export
+admm_scad = function(x, y, intercept = TRUE, standardize = TRUE, ...)
 {
-    # default parameters
-    opts_admm = list(maxit = 10000L,
-                     eps_abs = 1e-5,
-                     eps_rel = 1e-5,
-                     rho_ratio = 0.1)
-    # update from opts
-    opts_admm[names(opts)] = opts
-    
-    .Call("admm_scad", as.matrix(x), as.numeric(y), as.numeric(lambda),
-          as.integer(nlambda), as.numeric(lambda_min_ratio),
-          as.logical(standardize), as.logical(intercept),
-          as.numeric(pen_a),
-          as.list(opts_admm), PACKAGE = "ADMM")
+    ADMM_SCAD(x, y, intercept, standardize, ...)
 }
-
