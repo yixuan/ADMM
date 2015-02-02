32449afadba073706f5c593fcec4086d1353a223
Yixuan Qiu <yixuan.qiu@cos.name>
2015-02-01 19:55:46 -0500

    R function admm_scad()

---
+++ R/admm_scad.R
@@ -0,0 +1,22 @@
+admm_scad = function(x, y, lambda = NULL,
+                     nlambda = 100,
+                     lambda_min_ratio = ifelse(nrow(x) < ncol(x), 0.01, 0.0001),
+                     standardize = TRUE, intercept = TRUE,
+                     pen_a = 3.7,
+                     opts = list())
+{
+    # default parameters
+    opts_admm = list(maxit = 10000L,
+                     eps_abs = 1e-5,
+                     eps_rel = 1e-5,
+                     rho_ratio = 0.1)
+    # update from opts
+    opts_admm[names(opts)] = opts
+    
+    .Call("admm_scad", as.matrix(x), as.numeric(y), as.numeric(lambda),
+          as.integer(nlambda), as.numeric(lambda_min_ratio),
+          as.logical(standardize), as.logical(intercept),
+          as.numeric(pen_a),
+          as.list(opts_admm), PACKAGE = "ADMM")
+}
+
