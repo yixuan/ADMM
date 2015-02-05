028adcd3b5c01d3410985fb93edb45c05e42d654
Yixuan Qiu <yixuan.qiu@cos.name>
2015-02-04 22:29:15 -0500

    documentation for Basis Pursuit

--- R/10_admm_bp.R
+++ R/10_admm_bp.R
@@ -131,8 +131,10 @@ ADMM_BP_fit$methods(
         coefs = as.numeric(.self$beta)
         dat = data.frame(Index = seq_along(coefs),
                          Coefficients = coefs)
-        ggplot(dat, aes(x = Index, y = Coefficients)) +
+        g = ggplot(dat, aes(x = Index, y = Coefficients)) +
             geom_segment(aes(xend = Index, yend = 0))
+        print(g)
+        invisible(g)
     }
 )
 
@@ -140,6 +142,106 @@ ADMM_BP_fit$methods(
 
 
 
+#' Fitting A Basis Pursuit Model Using ADMM Algorithm
+#' 
+#' @description Basis Pursuit is an optimization problem that minimizes
+#' \eqn{\Vert \beta \Vert_1}{||\beta||_1} subject to
+#' \eqn{y=X\beta}{y = X * \beta}. Here \eqn{X} is an \eqn{n} by \eqn{p}
+#' matrix with \eqn{p > n}. Basis Pursuit is broadly applied in Compressed
+#' Sensing to recover a sparse vector \eqn{\beta} from the transformed
+#' lower dimensional vector \eqn{y}.
+#' 
+#' This function will not directly conduct the computation,
+#' but rather returns an object of class "\code{ADMM_BP}" that contains
+#' several memeber functions to actually constructs and fits the model.
+#' 
+#' Member functions that are callable from this object are listed below:
+#' 
+#' \tabular{ll}{
+#'   \code{$opts()}     \tab Setting additional options. See section
+#'                           \strong{Additional Options} for details.\cr
+#'   \code{$fit()}      \tab Fit the model and do the actual computation.
+#'                           See section \strong{Model Fitting} for details.
+#' }
+#' 
+#' @param x The transformation matrix
+#' @param y The transformed vector to recover from
+#' 
+#' @section Additional Options:
+#' Additional options related to ADMM algorithm can be set through the
+#' \code{$opts()} member function of an "\code{ADMM_BP}" object. The usage of
+#' this method is
+#' 
+#' \preformatted{    model$opts(maxit = 10000, eps_abs = 1e-4, eps_rel = 1e-4,
+#'                rho_ratio = 0.1)
+#' }
+#' 
+#' Here \code{model} is the object returned by \code{admm_bp()}.
+#' Explanation of the arguments is given below:
+#' 
+#' \describe{
+#' \item{\code{maxit}}{Maximum number of iterations.}
+#' \item{\code{eps_abs}}{Absolute tolerance parameter.}
+#' \item{\code{eps_rel}}{Relative tolerance parameter.}
+#' \item{\code{rho_ratio}}{ADMM step size parameter.}
+#' }
+#' 
+#' This member function will implicitly return the "\code{ADMM_BP}" object itself.
+#' 
+#' @section Model Fitting:
+#' Model will be fit after calling the \code{$fit()} member function. This is no
+#' argument that needs to be set. The function will return an object of class
+#' "\code{ADMM_BP_fit}", which contains the following fields:
+#' 
+#' \describe{
+#' \item{\code{beta}}{The recovered \eqn{\beta} vector in sparse form.}
+#' \item{\code{niter}}{Number of ADMM iterations.}
+#' }
+#' 
+#' Class "\code{ADMM_BP_fit}" also contains a \code{$plot()} member function,
+#' which plots the coefficients against their indices. See the examples below.
+#' 
+#' @examples
+#' ## An Compressed Sensing example ##
+#' 
+#' ## Create a sparse signal vector
+#' set.seed(123)
+#' n = 50
+#' p = 100
+#' nsig = 15
+#' beta_true = c(runif(nsig), rep(0, p - nsig))
+#' beta_true = sample(beta_true)
+#' 
+#' ## Generate the transformation matrix and the compressed vector
+#' x = matrix(rnorm(n * p), n, p)
+#' y = drop(x %*% beta_true)
+#' 
+#' ## Build the model
+#' model = admm_bp(x, y)
+#' 
+#' ## Request a higher precision
+#' model$opts(eps_rel = 1e-5)
+#' 
+#' ## Fit the model
+#' res = model$fit()
+#' res
+#' 
+#' ## Plot for the recovered vector
+#' res$plot()
+#' 
+#' ## The steps above can be accoumplished using a chainable call
+#' admm_bp(x, y)$opts(eps_rel = 1e-5)$fit()$plot()
+#' 
+#' ## Compare the true beta and the recovered one
+#' library(ggplot2)
+#' g = res$plot()
+#' d = data.frame(ind = seq_along(beta_true),
+#'                coef = beta_true)
+#' g + geom_segment(aes(x = ind + 0.5, xend = ind + 0.5,
+#'                      y = coef, yend = 0), data = d, color = "red")
+#' 
+#' @author Yixuan Qiu <\url{http://statr.me}>
+#' @export
 admm_bp = function(x, y, ...)
 {
     ADMM_BP(x, y, ...)
