f82f35346b28e4d73160b0b407aa5209366beef2
Yixuan Qiu <yixuan.qiu@cos.name>
2015-08-30 15:42:46 -0400

    minor change

--- src/Linalg/BlasWrapper.h
+++ src/Linalg/BlasWrapper.h
@@ -1,7 +1,7 @@
 #ifndef BLASWRAPPER_H
 #define BLASWRAPPER_H
 
-#include <Eigen/Dense>
+#include <Eigen/Core>
 
 namespace Linalg {
 
@@ -25,23 +25,42 @@ inline void mat_vec_prod(Eigen::VectorXd &res, const Eigen::MatrixXd &X, const E
 {
     const double one = 1.0;
     const double zero = 0.0;
-    const char no_trans = 'N';
     const int n = X.rows();
     const int p = X.cols();
     const int inc = 1;
 
     res.resize(n);
-    dgemv_(&no_trans, &n, &p, &one, X.data(), &n, v.data(), &inc, &zero, res.data(), &inc);
+    dgemv_("N", &n, &p, &one, X.data(), &n, v.data(), &inc, &zero, res.data(), &inc);
 }
 
 inline void mat_vec_prod(double *res, const double *X, const double *v, int n, int p)
 {
     const double one = 1.0;
     const double zero = 0.0;
-    const char no_trans = 'N';
     const int inc = 1;
 
-    dgemv_(&no_trans, &n, &p, &one, X, &n, v, &inc, &zero, res, &inc);
+    dgemv_("N", &n, &p, &one, X, &n, v, &inc, &zero, res, &inc);
+}
+
+inline void mat_vec_tprod(Eigen::VectorXd &res, const Eigen::MatrixXd &X, const Eigen::VectorXd &v)
+{
+    const double one = 1.0;
+    const double zero = 0.0;
+    const int n = X.rows();
+    const int p = X.cols();
+    const int inc = 1;
+
+    res.resize(p);
+    dgemv_("T", &n, &p, &one, X.data(), &n, v.data(), &inc, &zero, res.data(), &inc);
+}
+
+inline void mat_vec_tprod(double *res, const double *X, const double *v, int n, int p)
+{
+    const double one = 1.0;
+    const double zero = 0.0;
+    const int inc = 1;
+
+    dgemv_("T", &n, &p, &one, X, &n, v, &inc, &zero, res, &inc);
 }
 
 // Calculating X'X
@@ -68,8 +87,6 @@ inline void cross_prod_lower(Eigen::MatrixXd &res, const Eigen::Map<const Eigen:
 {
     const double one = 1.0;
     const double zero = 0.0;
-    const char trans = 'T';
-    const char uplo = 'L';
 
     const int n = X.rows();
     const int p = X.cols();
@@ -78,7 +95,7 @@ inline void cross_prod_lower(Eigen::MatrixXd &res, const Eigen::Map<const Eigen:
     res.resize(p, p);
     double *res_ptr = res.data();
 
-    dsyrk_(&uplo, &trans, &p, &n,
+    dsyrk_("L", "T", &p, &n,
            &one, x_ptr, &n,
            &zero, res_ptr, &p);
 }
@@ -87,8 +104,6 @@ inline void tcross_prod_lower(Eigen::MatrixXd &res, const Eigen::Map<const Eigen
 {
     const double one = 1.0;
     const double zero = 0.0;
-    const char no_trans = 'N';
-    const char uplo = 'L';
 
     const int n = X.rows();
     const int p = X.cols();
@@ -97,7 +112,7 @@ inline void tcross_prod_lower(Eigen::MatrixXd &res, const Eigen::Map<const Eigen
     res.resize(n, n);
     double *res_ptr = res.data();
 
-    dsyrk_(&uplo, &no_trans, &n, &p,
+    dsyrk_("L", "N", &n, &p,
            &one, x_ptr, &n,
            &zero, res_ptr, &n);
 }
