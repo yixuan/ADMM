695fc062baf65302afa2d146b26e068fdcfcafca
Yixuan Qiu <yixuan.qiu@cos.name>
2015-09-13 22:17:36 -0400

    make DataStd a template class

--- src/DataStd.h
+++ src/DataStd.h
@@ -7,15 +7,16 @@
 #include "AVX.h"
 #endif
 
+template <typename Scalar = double>
 class DataStd
 {
 private:
-    typedef Eigen::MatrixXd MatrixXd;
-    typedef Eigen::VectorXd VectorXd;
-    typedef Eigen::ArrayXd ArrayXd;
-    typedef const Eigen::Ref<const VectorXd> ConstGenericVector;
-    typedef Eigen::Ref<ArrayXd> Ref;
-    typedef Eigen::SparseVector<double> SparseVector;
+    typedef Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic> Matrix;
+    typedef Eigen::Matrix<Scalar, Eigen::Dynamic, 1> Vector;
+    typedef Eigen::Array <Scalar, Eigen::Dynamic, 1> Array;
+    typedef const Eigen::Ref<const Vector> ConstGenericVector;
+    typedef Eigen::Ref<Array> ArrayRef;
+    typedef Eigen::SparseVector<Scalar> SparseVector;
 
     // flag - 0: standardize = FALSE, intercept = FALSE
     //             directly fit model
@@ -30,10 +31,47 @@ private:
     const int n;
     const int p;
 
-    double meanY;
-    double scaleY;
-    ArrayXd meanX;
-    ArrayXd scaleX;
+    Scalar meanY;
+    Scalar scaleY;
+    Array  meanX;
+    Array  scaleX;
+
+    static Scalar sd_n(ConstGenericVector &v)
+    {
+#ifdef __AVX__
+        Scalar s, ss;
+        const int vsize = v.size();
+        get_ss_avx(v.data(), v.size(), s, ss);
+        s /= vsize;
+        return std::sqrt(ss / vsize - s * s);
+#else
+        Scalar mean = v.mean();
+        Vector v_centered = v.array() - mean;
+
+        return v_centered.norm() / std::sqrt(Scalar(v.size()));
+#endif
+    }
+
+    // spvec -> spvec / arr, elementwise
+    static void elementwise_quot(SparseVector &spvec, Array &arr)
+    {
+        for(typename SparseVector::InnerIterator iter(spvec); iter; ++iter)
+        {
+            iter.valueRef() /= arr[iter.index()];
+        }
+    }
+
+    // inner product of spvec and arr
+    static Scalar inner_product(SparseVector &spvec, Array &arr)
+    {
+        Scalar res = 0.0;
+        for(typename SparseVector::InnerIterator iter(spvec); iter; ++iter)
+        {
+            res += iter.value() * arr[iter.index()];
+        }
+        return res;
+    }
+
 public:
     DataStd(int n_, int p_, bool standardize, bool intercept) :
         flag(int(standardize) + 2 * int(intercept)),
@@ -48,25 +86,9 @@ public:
             scaleX.resize(p);
     }
 
-    static double sd_n(ConstGenericVector &v)
+    void standardize(Matrix &X, Vector &Y)
     {
-#ifdef __AVX__
-        double s, ss;
-        const int vsize = v.size();
-        get_ss_avx(v.data(), v.size(), s, ss);
-        s /= vsize;
-        return std::sqrt(ss / vsize - s * s);
-#else
-        double mean = v.mean();
-        VectorXd v_centered = v.array() - mean;
-
-        return v_centered.norm() / std::sqrt(double(v.size()));
-#endif
-    }
-
-    void standardize(MatrixXd &X, VectorXd &Y)
-    {
-        double n_invsqrt = 1.0 / std::sqrt(double(n));
+        Scalar n_invsqrt = 1.0 / std::sqrt(Scalar(n));
 
         // standardize Y
         switch(flag)
@@ -111,19 +133,19 @@ public:
                     scaleX[i] = X.col(i).norm() * n_invsqrt;
                     X.col(i).array() /= scaleX[i];*/
     #ifdef __AVX__
-                    double *begin = &X(0, i);
-                    double s, ss;
+                    Scalar *begin = &X(0, i);
+                    Scalar s, ss;
                     get_ss_avx(begin, n, s, ss);
                     meanX[i] = s / n;
                     scaleX[i] = std::sqrt(ss - s * s / n) * n_invsqrt;
                     standardize_vec_avx(begin, n, meanX[i], 1.0 / scaleX[i]);
     #else
-                    double *begin = &X(0, i);
-                    double *end = begin + n;
+                    Scalar *begin = &X(0, i);
+                    Scalar *end = begin + n;
                     meanX[i] = X.col(i).mean();
-                    std::transform(begin, end, begin, std::bind2nd(std::minus<double>(), meanX[i]));
+                    std::transform(begin, end, begin, std::bind2nd(std::minus<Scalar>(), meanX[i]));
                     scaleX[i] = X.col(i).norm() * n_invsqrt;
-                    std::transform(begin, end, begin, std::bind2nd(std::multiplies<double>(), 1.0 / scaleX[i]));
+                    std::transform(begin, end, begin, std::bind2nd(std::multiplies<Scalar>(), 1.0 / scaleX[i]));
     #endif
                 }
                 break;
@@ -132,7 +154,7 @@ public:
         }
     }
 
-    void recover(double &beta0, Ref coef)
+    void recover(Scalar &beta0, ArrayRef coef)
     {
         switch(flag)
         {
@@ -158,29 +180,7 @@ public:
         }
     }
 
-    // spvec -> spvec / arr, elementwise
-    static void elementwise_quot(Eigen::SparseVector<double> &spvec,
-                                 Eigen::ArrayXd &arr)
-    {
-        for(Eigen::SparseVector<double>::InnerIterator iter(spvec); iter; ++iter)
-        {
-            iter.valueRef() /= arr[iter.index()];
-        }
-    }
-
-    // inner product of spvec and arr
-    static double inner_product(Eigen::SparseVector<double> &spvec,
-                                Eigen::ArrayXd &arr)
-    {
-        double res = 0.0;
-        for(Eigen::SparseVector<double>::InnerIterator iter(spvec); iter; ++iter)
-        {
-            res += iter.value() * arr[iter.index()];
-        }
-        return res;
-    }
-
-    void recover(double &beta0, SparseVector &coef)
+    void recover(Scalar &beta0, SparseVector &coef)
     {
         switch(flag)
         {
@@ -206,7 +206,7 @@ public:
         }
     }
 
-    double get_scaleY() { return scaleY; }
+    Scalar get_scaleY() { return scaleY; }
 };
 
 
