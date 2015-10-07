7e423ca0183b5eac3e781f65cbb1a04b6d3c5d4f
Yixuan Qiu <yixuan.qiu@cos.name>
2015-10-06 20:37:38 -0400

    update enet code

--- src/Enet.cpp
+++ src/Enet.cpp
@@ -3,10 +3,11 @@
 #include "ADMMEnet.h"
 #include "DataStd.h"
 
-using Eigen::MatrixXd;
-using Eigen::VectorXd;
+using Eigen::MatrixXf;
+using Eigen::VectorXf;
+using Eigen::ArrayXf;
 using Eigen::ArrayXd;
-using Eigen::ArrayXXd;
+using Eigen::ArrayXXf;
 
 using Rcpp::wrap;
 using Rcpp::as;
@@ -14,13 +15,10 @@ using Rcpp::List;
 using Rcpp::Named;
 using Rcpp::IntegerVector;
 
-typedef Eigen::Map<MatrixXd> MapMat;
-typedef Eigen::Map<VectorXd> MapVec;
-typedef Eigen::Map<ArrayXd>  MapArray;
-typedef Eigen::SparseVector<double> SpVec;
-typedef Eigen::SparseMatrix<double> SpMat;
+typedef Eigen::SparseVector<float> SpVec;
+typedef Eigen::SparseMatrix<float> SpMat;
 
-inline void write_beta_matrix(SpMat &betas, int col, double beta0, SpVec &coef)
+inline void write_beta_matrix(SpMat &betas, int col, float beta0, SpVec &coef)
 {
     betas.insert(0, col) = beta0;
 
@@ -37,59 +35,46 @@ RcppExport SEXP admm_enet(SEXP x_, SEXP y_, SEXP lambda_,
 {
 BEGIN_RCPP
 
-#if ADMM_PROFILE > 0
-    double t1, t2;
-    t1 = omp_get_wtime();
-#endif
+    Rcpp::NumericMatrix xx(x_);
+    Rcpp::NumericVector yy(y_);
 
-    MatrixXd datX(as<MatrixXd>(x_));
-    VectorXd datY(as<VectorXd>(y_));
+    const int n = xx.rows();
+    const int p = xx.cols();
+
+    MatrixXf datX(n, p);
+    VectorXf datY(n);
+
+    // Copy data and convert type from double to float
+    std::copy(xx.begin(), xx.end(), datX.data());
+    std::copy(yy.begin(), yy.end(), datY.data());
 
     // In glmnet, we minimize
     //   1/(2n) * ||y - X * beta||^2 + lambda * ||beta||_1
     // which is equivalent to minimizing
     //   1/2 * ||y - X * beta||^2 + n * lambda * ||beta||_1
-    int n = datX.rows();
-    int p = datX.cols();
     ArrayXd lambda(as<ArrayXd>(lambda_));
     int nlambda = lambda.size();
 
     List opts(opts_);
-    int maxit = as<int>(opts["maxit"]);
-    double eps_abs = as<double>(opts["eps_abs"]);
-    double eps_rel = as<double>(opts["eps_rel"]);
-    double rho = as<double>(opts["rho"]);
+    const int maxit        = as<int>(opts["maxit"]);
+    const double eps_abs   = as<double>(opts["eps_abs"]);
+    const double eps_rel   = as<double>(opts["eps_rel"]);
+    const double rho       = as<double>(opts["rho"]);
+    const double alpha     = as<double>(alpha_);
+    const bool standardize = as<bool>(standardize_);
+    const bool intercept   = as<bool>(intercept_);
 
-    double alpha = as<double>(alpha_);
-
-    bool standardize = as<bool>(standardize_);
-    bool intercept = as<bool>(intercept_);
-
-#if ADMM_PROFILE > 0
-    t2 = omp_get_wtime();
-    Rcpp::Rcout << "part1: " << t2 - t1 << " secs.\n";
-#endif
-
-    DataStd datstd(n, p, standardize, intercept);
+    DataStd<float> datstd(n, p, standardize, intercept);
     datstd.standardize(datX, datY);
 
-#if ADMM_PROFILE > 0
-    t1 = omp_get_wtime();
-    Rcpp::Rcout << "part2: " << t1 - t2 << " secs.\n";
-#endif
-
-    ADMMEnet solver(datX, datY, alpha, eps_abs, eps_rel);
-
-#if ADMM_PROFILE > 0
-    t2 = omp_get_wtime();
-    Rcpp::Rcout << "part3: " << t2 - t1 << " secs.\n";
-#endif
+    ADMMEnetTall *solver_tall = new ADMMEnetTall(datX, datY, alpha, eps_abs, eps_rel);
 
     if(nlambda < 1)
     {
-        double lmax = solver.get_lambda_zero() / n * datstd.get_scaleY();
+        double lmax = 0.0;
+        lmax = solver_tall->get_lambda_zero() / n * datstd.get_scaleY();
         double lmin = as<double>(lmin_ratio_) * lmax;
-        lambda.setLinSpaced(as<int>(nlambda_), log(lmax), log(lmin));
+        lambda.setLinSpaced(as<int>(nlambda_), std::log(lmax), std::log(lmin));
         lambda = lambda.exp();
         nlambda = lambda.size();
     }
@@ -104,21 +89,18 @@ BEGIN_RCPP
     {
         ilambda = lambda[i] * n / datstd.get_scaleY();
         if(i == 0)
-            solver.init(ilambda, rho);
+            solver_tall->init(ilambda, rho);
         else
-            solver.init_warm(ilambda);
+            solver_tall->init_warm(ilambda);
 
-        niter[i] = solver.solve(maxit);
-        SpVec res = solver.get_z();
-        double beta0 = 0.0;
+        niter[i] = solver_tall->solve(maxit);
+        SpVec res = solver_tall->get_z();
+        float beta0 = 0.0;
         datstd.recover(beta0, res);
         write_beta_matrix(beta, i, beta0, res);
     }
 
-#if ADMM_PROFILE > 0
-    t1 = omp_get_wtime();
-    Rcpp::Rcout << "part4: " << t1 - t2 << " secs.\n";
-#endif
+    delete solver_tall;
 
     beta.makeCompressed();
 
