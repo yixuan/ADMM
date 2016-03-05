bdb2929ef58bf6fcdcc67dab4a9058f819b83fbf
Yixuan Qiu <yixuan.qiu@cos.name>
2016-03-05 15:58:23 -0500

    no longer needed

--- src/ADMMMatOp.h
+++ src/ADMMMatOp.h
@@ -3,36 +3,6 @@
 
 #include <Eigen/Core>
 
-template <typename Scalar>
-class MatOpSymLower
-{
-private:
-    typedef Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic> Matrix;
-    typedef Eigen::Map<const Matrix> MapMat;
-    typedef Eigen::Map< Eigen::Matrix<Scalar, Eigen::Dynamic, 1> > MapVec;
-    typedef const Eigen::Ref<const Matrix> ConstGenericMatrix;
-
-    const MapMat mat;
-    const int n;
-
-public:
-    MatOpSymLower(ConstGenericMatrix &mat_) :
-        mat(mat_.data(), mat_.rows(), mat_.cols()),
-        n(mat.rows())
-    {}
-
-    int rows() { return n; }
-    int cols() { return n; }
-
-    // y_out = A * x_in
-    void perform_op(Scalar *x_in, Scalar *y_out)
-    {
-        MapVec x(x_in, n);
-        MapVec y(y_out, n);
-        y.noalias() = mat.template selfadjointView<Eigen::Lower>() * x;
-    }
-};
-
 
 template <typename Scalar>
 class MatOpXX
